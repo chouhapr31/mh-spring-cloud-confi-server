23858c72a1e07cb851fa22f058be5d3c943ee647
chouhapr <prashant.chouhan@globalpay.com>
2020-04-25 09:19:27 -0700

    spring cloud config server

---
+++ src/test/java/com/mh/config/server/MhConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
